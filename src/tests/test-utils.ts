ecf10c3f55d79ed08683f828fe03b2547a2f74eb
djancool016 <dwijulianto16@gmail.com>
2025-04-11 13:45:37 +0700

    add randomize functions

--- src/tests/test-utils.ts
+++ src/tests/test-utils.ts
@@ -17,3 +17,11 @@ export function createMockResponse(overrides: Partial<Response> = {}): any {
     };
     return res;
 }
+
+export function randomNumber(length: number): number{
+    return Math.floor(Math.random() * (10**length - 10**(length-1))) + 10**(length-1)
+}
+
+export function randomString(length: number): string{
+    return Array.from({length: length}, () => Math.floor(Math.random() * 36).toString(36)).join('')
+}
