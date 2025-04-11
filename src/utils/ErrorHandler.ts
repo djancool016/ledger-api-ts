3a950fa8da5dbab6b5a1ddd8007343f0ef2c8dc6
djancool016 <dwijulianto16@gmail.com>
2025-04-11 13:45:10 +0700

    remove console error

--- src/utils/ErrorHandler.ts
+++ src/utils/ErrorHandler.ts
@@ -4,11 +4,9 @@ import { z } from "zod";
 
 export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
     if (err instanceof AppError) {
-        console.error(`${err.name}: ${err.message}`)
         return res.status(err.statusCode).json({ message: err.message });
     } else if(err instanceof z.ZodError){
         const message = err.errors.map( e => e.message ).join(', ')
-        console.error(`BadRequestError: ${message}`)
         return res.status(400).json({message})
     }
     console.error(err); // Log unexpected errors for debugging
